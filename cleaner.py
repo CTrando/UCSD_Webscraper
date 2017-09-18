24bd185e47e309eaa959c266cffcc47496f7ed94
Cameron <cameron.trando@yahoo.com>
2017-09-18 13:10:52 -0700

    Finally linked together lectures, labs, discussions and finals. Could be wrong though will test further.

---
+++ cleaner.py
@@ -0,0 +1,63 @@
+import sqlite3
+
+real_data = sqlite3.connect('real_data.db')
+r_cursor = real_data.cursor()
+r_cursor.execute('DROP TABLE DATA')
+r_cursor.execute(
+    'CREATE TABLE IF NOT EXISTS DATA (COURSE_NUM TEXT, LECTURE_KEY INTEGER, LAB_KEY INTEGER, DISCUSSION_KEY INTEGER, FINAL_KEY INTEGER)')
+
+
+class Class:
+    def __init__(self):
+        self.course_num = None
+        self.lab_key = None
+        self.lecture_key = None
+        self.discussion_key = None
+        self.final_key = None
+
+    @staticmethod
+    def get_type(row):
+        for col in row:
+            if col in ('LE', 'LA', 'DI', 'FINAL'):
+                return col
+        return None
+
+    @staticmethod
+    def insert_discussion(course_num, discussion_id):
+        r_cursor.execute("UPDATE DATA SET DISCUSSION_KEY = ? WHERE COURSE_NUM = ? AND DISCUSSION_KEY IS NULL", (discussion_id, course_num))
+
+    @staticmethod
+    def insert_lab(course_num, lab_id):
+        r_cursor.execute("UPDATE DATA SET LAB_KEY = ? WHERE COURSE_NUM = ? AND LAB_KEY IS NULL", (lab_id, course_num))
+
+
+    @staticmethod
+    def insert_final(course_num, final_id):
+        r_cursor.execute("UPDATE DATA SET FINAL_KEY = ? WHERE COURSE_NUM = ? AND FINAL_KEY IS NULL", (final_id, course_num))
+
+    def save(self):
+        r_cursor.execute("INSERT INTO DATA VALUES(?,?,?,?,?)", (self.course_num, self.lecture_key, self.lab_key, self.discussion_key, self.final_key))
+
+
+database = sqlite3.connect('data.db')
+cursor = database.cursor()
+cursor.execute("SELECT DISTINCT COURSE_NUM FROM CLASSES")
+for num in cursor.fetchall():
+    cursor.execute("SELECT ID, COURSE_ID, TYPE FROM CLASSES WHERE COURSE_NUM = ?", num)
+    for thing in cursor.fetchall():
+        print(thing)
+        if Class.get_type(thing) == 'LE':
+            new_class = Class()
+            new_class.course_num = num[0]
+            new_class.lecture_key = thing[0]
+            new_class.save()
+        elif Class.get_type(thing) == 'DI':
+            Class.insert_discussion(num[0], thing[0])
+        elif Class.get_type(thing) == 'FINAL':
+            Class.insert_final(num[0], thing[0])
+        elif Class.get_type(thing) == 'LA':
+            Class.insert_lab(num[0], thing[0])
+    print('*' * 10)
+
+real_data.commit()
+real_data.close()
--- parser.py
+++ parser.py
@@ -1,63 +1,4 @@
-import bs4, os, sqlite3
-
-preset_days = [
-    'M', 'Tu', 'W', 'Th', 'F'
-]
-
-current_lecture = None
-
-
-def has_word(buffer, word):
-    for sentence in buffer:
-        if word in sentence:
-            return True
-    return False
-
-
-def made_of_days(section):
-    section_len = len(section)
-    for day in preset_days:
-        if day in section:
-            section_len -= len(day)
-    return section_len == 0
-
-
-def get_info(info):
-    department = info[0]
-    course_num = info[1]
-    type = 'TBA'
-    location = 'TBA'
-    time = 'TBA'
-    instructor = 'TBA'
-    days = 'TBA'
-    if 'FINAL' in info:
-        type = 'FINAL'
-    elif 'LA' in info:
-        type = 'LAB'
-    elif 'LE' in info:
-        type = 'LECTURE'
-        current_lecture = info
-    elif 'DI' in info:
-        type = 'DISCUSSION'
-        instructor = 'N/A'
-    elif 'PB' in info:
-        type = 'PROBLEM SESSION'
-
-    for i in range(0, len(info)):
-        section = info[i]
-        if made_of_days(section):
-            days = section
-
-        if ':' in section and '(' not in section:
-            time = section
-            location = info[i + 1]
-            i = i + 1
-        if ',' in section:
-            instructor = section
-
-    description = info[-1]
-    return (department, course_num, type, days, time, location, instructor, description)
-
+import bs4, os, sqlite3, re
 
 HTML_STORAGE = 'classes'
 
@@ -69,34 +10,50 @@ for root, dirs, files in os.walk(os.curdir):
     for file in files:
         with open(file) as html:
             soup = bs4.BeautifulSoup(html, 'lxml')
-            for string in soup.stripped_strings:
-                if 'CSE' == string:
-                    if not has_word(buffer, 'Note') and '|' not in buffer and not has_word(buffer, 'Enrolled') \
-                            and not has_word(buffer, 'Section') and not has_word(buffer, 'cancelled'):
-                        print(buffer)
-                        buffer_buffer.append(get_info(buffer))
-                    buffer = []
-                buffer.append(string)
+            rows = soup.find_all(name='tr')
+            for row in rows:
+                header = row.find(name='table', attrs={'id': 'search-group-header-id'})
+                section_id = row.find(name='td', attrs={'role': 'gridcell',
+                                                        'aria-describedby': 'search-div-b-table_SECTION_NUMBER'})
+                lecture_type = row.find(name='td', attrs={'role': 'gridcell',
+                                                          'aria-describedby': 'search-div-b-table_FK_CDI_INSTR_TYPE'})
+                day = row.find(name='td', attrs={'role': 'gridcell', 'aria-describedby': 'search-div-b-table_DAY_CODE'})
+                time = row.find(name='td', attrs={'role': 'gridcell', 'aria-describedby': 'search-div-b-table_coltime'})
+                location = row.find(name='td',
+                                    attrs={'role': 'gridcell', 'aria-describedby': 'search-div-b-table_BLDG_CODE'})
+                room = row.find(name='td',
+                                attrs={'role': 'gridcell', 'aria-describedby': 'search-div-b-table_ROOM_CODE'})
+                instructor = row.find(name='td', attrs={'role': 'gridcell',
+                                                        'aria-describedby': 'search-div-b-table_PERSON_FULL_NAME'})
+
+                if not None in (header, section_id, lecture_type, day, time, location, room, instructor):
+                    name_desc = header.find_all(name='td')
+
+                    course_department = 'CSE'
+                    name = ' '.join(name_desc[0].text.split())
+                    section_id = ' '.join(section_id.text.split())
+                    lecture_type = ' '.join(lecture_type.text.split())
+                    day = ' '.join(day.text.split())
+                    time = ' '.join(time.text.split())
+                    location = ' '.join(location.text.split())
+                    room = ' '.join(room.text.split())
+                    instructor = ' '.join(instructor.text.split())
+                    description = ' '.join(name_desc[1].text.split())
+
+                    print(name, description, section_id, lecture_type, day, time, location, room, instructor)
+
+                    print('*' * 10)
+                    vals = (None, name, section_id, lecture_type, day, time, location, room, instructor, description)
+                    buffer_buffer.append(vals)
 
 os.chdir('C:/Users/ctran/PycharmProjects/UCSD_Webscraper')
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
-
 cursor.execute("DROP TABLE CLASSES")
-
 cursor.execute("CREATE TABLE CLASSES" +
-               "  (ID INTEGER PRIMARY KEY, PARENT_KEY INTEGER, COURSE_DEPARTMENT text, COURSE_NUM real, TYPE text, DAYS text, TIME text, LOCATION text, INSTRUCTOR text, DESCRIPTION text)")
+               "  (ID INTEGER PRIMARY KEY, COURSE_NUM real, COURSE_ID text, TYPE text, DAYS text, TIME text, LOCATION text, ROOM text, INSTRUCTOR text, DESCRIPTION text)")
 
-current_row = 0
-for thing in buffer_buffer:
-    if 'LECTURE' in thing:
-        current_row = None
-    cursor.execute("INSERT INTO CLASSES VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (None, current_row)+ thing)
-    if 'LECTURE' in thing:
-        current_row = cursor.lastrowid
-# Save (commit) the changes
+for vals in buffer_buffer:
+    cursor.execute("INSERT INTO CLASSES VALUES(?,?,?,?,?,?,?,?, ?, ?)", vals)
 connection.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
 connection.close()
